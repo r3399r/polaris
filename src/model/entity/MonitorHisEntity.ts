eb85570439e2034947b71cf40b5e03d3474e6d1f
yue.chang <lamplighter.planet@gmail.com>
2024-09-26 21:51:59 +0800

    improve performance and consider fail job

--- src/model/entity/MonitorHisEntity.ts
+++ src/model/entity/MonitorHisEntity.ts
@@ -3,8 +3,9 @@ import { BeforeInsert, Column, Entity, Generated } from 'typeorm';
 export type MonitorHis = {
   id: string;
   name: string;
-  rowCount: string;
-  elapsedTime: number;
+  rowCount: string | null;
+  elapsedTime: number | null;
+  success: boolean;
   dateCreated: string;
 };
 
@@ -17,11 +18,14 @@ export class MonitorHisEntity implements MonitorHis {
   @Column({ type: 'text' })
   name!: string;
 
-  @Column({ type: 'int', name: 'row_count' })
-  rowCount!: string;
+  @Column({ type: 'int', name: 'row_count', default: null })
+  rowCount: string | null = null;
 
-  @Column({ type: 'float', name: 'elapsed_time' })
-  elapsedTime!: number;
+  @Column({ type: 'float', name: 'elapsed_time', default: null })
+  elapsedTime: number | null = null;
+
+  @Column({ type: 'bool' })
+  success!: boolean;
 
   @Column({ type: 'timestamp', name: 'date_created', default: null })
   dateCreated!: string;
