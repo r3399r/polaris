10cd9d7a72fee2703f95e0217660ea3850341a8a
yue.chang <lamplighter.planet@gmail.com>
2024-10-09 22:24:46 +0800

    improve delete performance

--- src/logic/HouseKeepingService.ts
+++ src/logic/HouseKeepingService.ts
@@ -1,8 +1,5 @@
 import { inject, injectable } from 'inversify';
-import { Raw } from 'typeorm';
 import { DbAccess } from 'src/access/DbAccess';
-import { LogApiAccess } from 'src/access/LogApiAccess';
-import { MonitorHisAccess } from 'src/access/MonitorHisAccess';
 
 /**
  * Service class for House Keeping
@@ -12,28 +9,18 @@ export class HouseKeepingService {
   @inject(DbAccess)
   private readonly dbAccess!: DbAccess;
 
-  @inject(MonitorHisAccess)
-  private readonly monitorHisAccess!: MonitorHisAccess;
-
-  @inject(LogApiAccess)
-  private readonly logApiAccess!: LogApiAccess;
-
   public async houseKeep() {
     // clean cockroach db
     await this.dbAccess.resetSqlStats();
 
     // clean monitor history
-    await this.monitorHisAccess.hardDelete({
-      where: {
-        dateCreated: Raw((alias) => `${alias} < NOW() - interval '14 day'`),
-      },
-    });
+    await this.dbAccess.query(
+      "delete from monitor_his mh where mh.date_created < NOW() - interval '14 day'"
+    );
 
     // clean log of api
-    await this.logApiAccess.hardDelete({
-      where: {
-        dateCreated: Raw((alias) => `${alias} < NOW() - interval '7 day'`),
-      },
-    });
+    await this.dbAccess.query(
+      "delete from log_api la where la.date_created < NOW() - interval '7 day'"
+    );
   }
 }
