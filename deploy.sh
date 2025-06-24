919f39afc00776947603a8706a9c6cb0f41f3cbe
yue.chang <lamplighter.planet@gmail.com>
2025-06-24 23:36:42 +0800

    move to ap-east-2

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ echo ===========================================================================
 echo deploy backend AWS...
 npm i
 npm run pre:deploy
-aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway-singapore
-aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project --no-fail-on-empty-changeset --s3-bucket y-cf-midway-singapore --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway-ap-east-2
+aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project --no-fail-on-empty-changeset --s3-bucket y-cf-midway-ap-east-2 --capabilities CAPABILITY_NAMED_IAM
 echo ====================================================================================
